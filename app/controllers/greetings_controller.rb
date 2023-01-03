e5dc39293f672de8ccc35693a32fc5d1b88743b6
Federico Luna <fluna1973@yahoo.com>
2023-01-02 21:07:19 -0600

    fix linters

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,11 +1,6 @@
 class GreetingsController < ApplicationController
-  # before_action :set_greeting, only: %i[show edit update destroy]
-
   def random
-    # message = Greeting.where(id: rand(1..5))
-    # render json: { greetings: message }
-    random_number = 1 + rand(Greeting.count)
-    @mesage = Greeting.find(random_number)
-    render json: @mesage
+    @message = Greeting.find(rand(1..5))
+    render json: @message
   end
 end
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -13,7 +13,7 @@ const Greeting = () => {
 
   return (
     <>
-      <h1>Random Greeting:</h1>
+      <h1>This is a Random Greeting:</h1>
       <p>{content}</p>
       <button type="button" onClick={() => dispatch(fetchGreeting())}>
         generate new
@@ -23,82 +23,3 @@ const Greeting = () => {
 };
 
 export default Greeting;
-
-
-
-// import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchGreeting } from '../redux/greetings/greetingsSlice';
-
-// const Greeting = () => {
-//   const greetingShow = useSelector((state) => state.greetings.message);
-//   const status = useSelector((state) => state.greetings.status);
-//   const dispatch = useDispatch();
-//   let content;
-//   if (status === 'succeeded') {
-//     content = greetingShow.message;
-//   }
-
-
-//   return (
-//     <>
-//       <h1>Random Greeting:</h1>
-//       <h1>{content}</h1>
-//       <h1>this is a test</h1>
-//       <button type="button" onClick={() => dispatch(fetchGreeting())}>
-//         generate new
-//       </button>
-//     </>
-//   );
-// };
-
-// export default Greeting;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux"
-// import { fetchGreeting } from '../redux/greetings/greetingsSlice';
-
-// const Greeting = () =>{
-//     const [loading, setLoading] = useState(false)
-//     const dispatch = useDispatch();
-//     const greeting = useSelector(state => state.greeting)
-//     console.log({ greeting, loading})
-//     useEffect(() =>{
-//         if (loading) { return }
-//         setLoading(true)
-//         dispatch(fetchGreeting()).then(response => {
-//             setLoading(false)
-//             console.log({ response })
-//         }).catch(() => {
-//             setLoading(false)
-//         })
-//     }, []);
-
-//     return(
-//         <>
-//         <h1>{greeting.message}</h1>
-//         </>
-//     )
-// }
-
-// export default Greeting;
